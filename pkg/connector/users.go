c07211854423dfa9ef5630298c3550e91263493d
mchavez <miguel_chavez_m@hotmail.com>
2024-09-18 12:37:39 -0600

    Minor changes

--- pkg/connector/users.go
+++ pkg/connector/users.go
@@ -100,30 +100,12 @@ func (u *userBuilder) List(ctx context.Context, parentResourceID *v2.ResourceId,
 }
 
 // Entitlements always returns an empty slice for users.
-func (u *userBuilder) Entitlements(
-	_ context.Context,
-	resource *v2.Resource,
-	_ *pagination.Token,
-) (
-	[]*v2.Entitlement,
-	string,
-	annotations.Annotations,
-	error,
-) {
+func (u *userBuilder) Entitlements(_ context.Context, resource *v2.Resource, _ *pagination.Token) ([]*v2.Entitlement, string, annotations.Annotations, error) {
 	return nil, "", nil, nil
 }
 
 // Grants always returns an empty slice for users since they don't have any entitlements.
-func (u *userBuilder) Grants(
-	ctx context.Context,
-	resource *v2.Resource,
-	pToken *pagination.Token,
-) (
-	[]*v2.Grant,
-	string,
-	annotations.Annotations,
-	error,
-) {
+func (u *userBuilder) Grants(ctx context.Context, resource *v2.Resource, pToken *pagination.Token) ([]*v2.Grant, string, annotations.Annotations, error) {
 	return nil, "", nil, nil
 }
 
